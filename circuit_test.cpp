758e89f39394572727acaef301212bd6c9726618
Josh McConkie <bjmcconk@byu.edu>
2025-10-23 17:54:53 -0600

    init of circuit testing

---
+++ circuit_test.cpp
@@ -0,0 +1,15 @@
+#include "components.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+Node v1;
+Node v2;
+Node v3;
+Node vg;
+
+v1.to(v2).R = 1000;
+v1.connect(vg,R=10);
+
+
+vector<Node> nodes {}
